6366cd2700c163a9d1dd18fe8707cb966957d83e
gregfromstl <gregfromstl@gmail.com>
2022-04-12 12:41:48 -0500

    fix: safety latch tests

--- test/SafetyLatchUpgradeable.test.ts
+++ test/SafetyLatchUpgradeable.test.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 import { Contract, Signer } from "ethers";
-import { ethers, upgrades } from "hardhat";
+import { ethers } from "hardhat";
 
 const safetyLatchUpgradeableAbi = [
-  "function withdrawErc721(address contractAddress, uint256 tokenId) public",
-  "function withdrawErc1155(address contractAddress, uint256 tokenId) public",
-  "function withdrawErc20(address contractAddress) public",
+  "function withdrawERC721(address contractAddress, uint256 tokenId) public",
+  "function withdrawERC1155(address contractAddress, uint256 tokenId) public",
+  "function withdrawERC20(address contractAddress) public",
 ];
 
 let owner: Signer, alice: Signer, bob: Signer;
@@ -24,23 +24,23 @@ describe("Safety latch", function () {
   });
 
   it("allows only the owner to withdraw erc721s", async () => {
-    const erc721 = await givenErc721(safetyLatchUpgradeable.address, 0);
+    const erc721 = await givenERC721(safetyLatchUpgradeable.address, 0);
 
     const asAlice = new ethers.Contract(
       safetyLatchUpgradeable.address,
       safetyLatchUpgradeableAbi,
       alice
     );
-    await expect(asAlice.withdrawErc721(erc721.address, 0)).to.be.revertedWith(
+    await expect(asAlice.withdrawERC721(erc721.address, 0)).to.be.revertedWith(
       "Ownable: caller is not the owner"
     );
 
-    await safetyLatchUpgradeable.withdrawErc721(erc721.address, 0);
+    await safetyLatchUpgradeable.withdrawERC721(erc721.address, 0);
     expect(await erc721.ownerOf(0)).to.equal(await owner.getAddress());
   });
 
   it("allows only the owner to withdraw erc1155s", async () => {
-    const erc1155 = await givenErc1155(safetyLatchUpgradeable.address, 0, 10);
+    const erc1155 = await givenERC1155(safetyLatchUpgradeable.address, 0, 10);
 
     const asAlice = new ethers.Contract(
       safetyLatchUpgradeable.address,
@@ -48,10 +48,10 @@ describe("Safety latch", function () {
       alice
     );
     await expect(
-      asAlice.withdrawErc1155(erc1155.address, 0)
+      asAlice.withdrawERC1155(erc1155.address, 0)
     ).to.be.revertedWith("Ownable: caller is not the owner");
 
-    await safetyLatchUpgradeable.withdrawErc1155(erc1155.address, 0);
+    await safetyLatchUpgradeable.withdrawERC1155(erc1155.address, 0);
     expect(await erc1155.balanceOf(safetyLatchUpgradeable.address, 0)).to.equal(
       0
     );
@@ -59,24 +59,24 @@ describe("Safety latch", function () {
   });
 
   it("allows only the owner to withdraw erc20s", async () => {
-    const erc20 = await givenErc20(safetyLatchUpgradeable.address, 10);
+    const erc20 = await givenERC20(safetyLatchUpgradeable.address, 10);
 
     const asAlice = new ethers.Contract(
       safetyLatchUpgradeable.address,
       safetyLatchUpgradeableAbi,
       alice
     );
-    await expect(asAlice.withdrawErc20(erc20.address)).to.be.revertedWith(
+    await expect(asAlice.withdrawERC20(erc20.address)).to.be.revertedWith(
       "Ownable: caller is not the owner"
     );
 
-    await safetyLatchUpgradeable.withdrawErc20(erc20.address);
+    await safetyLatchUpgradeable.withdrawERC20(erc20.address);
     expect(await erc20.balanceOf(safetyLatchUpgradeable.address)).to.equal(0);
     expect(await erc20.balanceOf(await owner.getAddress())).to.equal(10);
   });
 });
 
-const givenErc721 = async (address: string, id: number) => {
+const givenERC721 = async (address: string, id: number) => {
   const ERC721 = await ethers.getContractFactory("MintableERC721");
   const erc721 = await ERC721.deploy();
   await erc721.deployed();
@@ -86,7 +86,7 @@ const givenErc721 = async (address: string, id: number) => {
   return erc721;
 };
 
-const givenErc1155 = async (address: string, id: number, amount: number) => {
+const givenERC1155 = async (address: string, id: number, amount: number) => {
   const ERC1155 = await ethers.getContractFactory("MintableERC1155");
   const erc1155 = await ERC1155.deploy("");
   await erc1155.deployed();
@@ -96,7 +96,7 @@ const givenErc1155 = async (address: string, id: number, amount: number) => {
   return erc1155;
 };
 
-const givenErc20 = async (address: string, amount: number) => {
+const givenERC20 = async (address: string, amount: number) => {
   const ERC20 = await ethers.getContractFactory("MintableERC20");
   const erc20 = await ERC20.deploy();
   await erc20.deployed();
