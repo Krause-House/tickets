ee6ebcc7fbaea149de5cbf285db44a792fe72d78
gregfromstl <gregfromstl@gmail.com>
2022-04-22 12:00:48 -0500

    deployed

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,11 @@ async function main() {
   const krauseTicketsContract = await upgrades.deployProxy(V2Tickets, [
     "0x6F1183aB121e9F05CA4916237012Ed37B6f20583", // THESE ARE THE MAINNET ADDRESSES
     "0xC4E0f3Ec24972C75dF7c716922096F4270b7bB4e",
-    "",
+    "https://gateway.pinata.cloud/ipfs/QmVGYouVCfxpsJKrecrNFmbeqom98Ybhrz5yL7K3kiVRMo/",
   ]);
   await krauseTicketsContract.deployed();
   console.log("KrauseTickets deployed to:", krauseTicketsContract.address);
 
-  await krauseTicketsContract.setUri(
-    "https://gateway.pinata.cloud/ipfs/QmVGYouVCfxpsJKrecrNFmbeqom98Ybhrz5yL7K3kiVRMo/"
-  );
   await krauseTicketsContract.setContractURI(
     "https://gateway.pinata.cloud/ipfs/QmfWTJPp173RT15YoEmRJUBJbGGigocgyLFiH8upWV8w7z/"
   );
